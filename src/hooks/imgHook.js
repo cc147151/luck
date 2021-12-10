a48c36df9ed2cdfe6dc8d3a10a1913d588074c35
眼睛微微笑 <44471027+cc147151@users.noreply.github.com>
2021-12-10 10:55:27 +0800

    Update imgHook.js
    fix:解决多张图片使用造成取值的问题

--- src/hooks/imgHook.js
+++ src/hooks/imgHook.js
@@ -1,22 +1,23 @@
-import { ref } from "vue";
+//首先图片是1:1比例，解决不同图片免去单独设置宽高
+// 在函数里创建ref，可以避免多张图片使用造成取值的问题
+import { ref } from "vue"
 // 使用解构视图是不会重新渲染的
-let imgStyle = ref({});
-const imgWidth = ref("");
-const imgHeight = ref("");
-const getSize = (naturalWidth, naturalHeight) => {
-  const clientWidth = document.documentElement.clientWidth;
-  const fontSize = clientWidth / 750;
-  imgWidth.value = naturalWidth * fontSize + "px";
-  imgHeight.value = naturalHeight * fontSize + "px";
-  imgStyle.value = { width: imgWidth.value, height: imgHeight.value };
-
-};
+const getSize = (naturalWidth, naturalHeight,imgWidth,imgHeight,imgStyle) => {
+  const clientWidth = document.documentElement.clientWidth
+  const fontSize = clientWidth / 750
+  imgWidth.value = naturalWidth * fontSize + "px"
+  imgHeight.value = naturalHeight * fontSize + "px"
+  imgStyle.value = { width: imgWidth.value, height: imgHeight.value }
+}
 export const imgHook = (e) => {
-  const { naturalWidth, naturalHeight } = e.path[0];
-  getSize(naturalWidth, naturalHeight);
-    imgStyle.value = { width: imgWidth.value, height: imgHeight.value };
+  let imgStyle = ref({})
+  const imgWidth = ref("")
+  const imgHeight = ref("")
+  const { naturalWidth, naturalHeight } = e.path[0]
+  getSize(naturalWidth, naturalHeight,imgWidth,imgHeight,imgStyle)
+  imgStyle.value = { width: imgWidth.value, height: imgHeight.value }
   window.addEventListener("resize", () => {
-    getSize(naturalWidth, naturalHeight);
-  });
-  return imgStyle;
-};
+    getSize(naturalWidth, naturalHeight,imgWidth,imgHeight,imgStyle)
+  })
+  return imgStyle
+}
